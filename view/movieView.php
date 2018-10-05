6dde9edd92a66a5205ae1894f05c98271ab382ed
claireb25 <claire.bourgeois.cb@gmail.com>
2018-10-05 21:49:31 +0200

    people page almost ready

--- view/movieView.php
+++ view/movieView.php
@@ -17,9 +17,9 @@
 
     <h2 class="title1"><?= $film["title"]?></h2>
     <?php $i = 0; ?>
-    <p>
+    <p class="margin-left">
     <?php foreach($allGenres as $genre) {?>
-        <a href="genre/<?php echo $allidGenres[$i]; ?>"><?php echo $genre?></a> 
+        <a class="link-style" href="genre/<?php echo $allidGenres[$i]; ?>"><?php echo $genre?></a> 
     <?php $i++; }?>
     </p>
     <section class="movie1">
@@ -27,19 +27,21 @@
     <p><img class ="poster" src="<?= $film["affiche"]?>"width="240" height="360"></p>
 </div>
 <div class="info">
-    <p class="year"><span>Film sorti en salle en<a href="annee/<?php echo $film['idYear']?>"><?= $film['year']?></a></span></p>
+    <p class="year"><span>Film sorti en salle en <a class="link-style" href="annee/<?php echo $film['idYear']?>"><?= $film['year']?></a></span></p>
     <p><span>Réalisé par</span>  <?php $j = 0; ?>
     <?php  foreach($allReals as $real){?>
-        <a href="personne/<?php echo $allidReals[$j];?>"> 
+        <a class="link-style" href="personne/<?php echo $allidReals[$j];?>"> 
     <?php echo $real; ?></a>  
     <?php $j++;} ?></p>
 
     <p><span>Avec </span><?php $k = 0; ?>
-                    <?php  foreach($allActors as $actor){?>
+
+                    <?php
+                      foreach($allActors as $actor){?>
                     
-                        <a href="personne/<?php echo $allidActors[$k];?>">
+                        <a class="link-style" href="personne/<?php echo $allidActors[$k];?>">
                             <?php echo $actor;
-                            $k++;}?> </a>
+                            $k++;}?></a>
                 
     <p class="synopsis">Synopsis</p>
     <p class="description"><?= $film["synopsis"]?></p>
--- view/peopleView.php
+++ view/peopleView.php
@@ -8,45 +8,58 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $person["nom_personne"]?></title>
     <base href="http://localhost/filmorama/">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/styleMovie.css">
 </head>
 <body>
+    
     <div class="backgroundHeader">
-        <h1>Filmorama</h1>
+    <h1> <a class="link-style-white" href="homepage">Filmorama</a></h1>
     </div>
-    <!-- Affichage infos personne -->
     <h2 class="title1"><?php echo $person["nom_personne"]?> </h2> 
     <section class="personne">
-        <div class="img">
-        <p><img class="poster portrait" src="<?php echo $person['portrait'];?>" width="240" height="360"></p>
-        </div>
-        <div class="info">
-            <?php 
-            if($age['death'] != NULL){?>
-                <p class="birth">Naissance : <?php setlocale(LC_TIME, "fr_FR", "French"); echo strftime("%d %B %G", strtotime($age['birth_date']));?></p>
-                <p class="age">Décès : <?php setlocale(LC_TIME, "fr_FR", "French"); echo strftime("%d %B %G", strtotime($age['death']));?> à l'âge de <?php echo $age['age_death']?> ans</p>
+        <div class="header-people">
+            <div class="img">
+                <p>
+                    <img class="poster portrait" src="<?php echo $person['portrait'];?>" width="240" height="360">
+                </p>
+            </div>
+            <div class="info">
                 <?php 
-           }else { ?> 
-                <p class="birth">Naissance : <?php setlocale(LC_TIME, "fr_FR", "French"); echo utf8_encode(strftime("%d %B %G", strtotime($age['birth_date']))); ?>
-                <p class="age">Âge : <?php echo $age['age'];?> ans 
-            <?php } ?></p>
+                if($age['death'] != NULL){?>
+                    <p class="birth">Naissance : <?php setlocale(LC_TIME, "fr_FR", "French"); echo strftime("%d %B %G", strtotime($age['birth_date']));?></p>
+                    <p class="age">Décès : <?php setlocale(LC_TIME, "fr_FR", "French"); echo strftime("%d %B %G", strtotime($age['death']));?> à l'âge de <?php echo $age['age_death']?> ans</p>
+                    <?php 
+            }else { ?> 
+                    <p class="birth">Naissance : <?php setlocale(LC_TIME, "fr_FR", "French"); echo utf8_encode(strftime("%d %B %G", strtotime($age['birth_date']))); ?>
+                    <p class="age">Âge : <?php echo $age['age'];?> ans 
+                <?php } ?></p>
+            
+            
+                <p class="nationalite">Nationalité : </p>
            
-          
-            <p class="nationalite">Nationalité : </p>
-            <h3>Biographie</h3>
-            <p class="biographie"><?php echo $person['biographie'];?></p>
+            <p>Biographie :</p>
+            <p><?php if (strlen($person['biographie'])>1400){
+            echo substr($person['biographie'],0,1400);
+            } else {
+                echo $person['biographie'];
+                }?></p>
+            </div>
         </div>
-    </section>
+  
 
     <!-- Affichage pour chaque film par acteur -->
-    <h3>Filmographie</h3>
-    <div class="movie">
+    <h3 class="title2">Filmographie</h3>
+    <div class="movies">
     <?php foreach($film as $row){?>
-        <h4><?php echo $row["titre"]?></h4>
-        <img src="<?php echo $row["affiche"];?>">
-        <p><a href="film/<?php echo $row['id_film'];?>">Découvrir</a></p>
+        <div class="movie">
+            <h4><?php echo $row["titre"]?></h4>
+            <img src="<?php echo $row["affiche"];?>" width="180" height="270">
+            <a class="stylebouton" href="film/<?php echo $row['id_film'];?>">Découvrir</a>
+        </div>
         <?php } ?>
     </div>
-   
+</section>
 </body>
 
 </html>
